f199d7bfb0d5e5ff33570e7b51c31e917bc5cbfc
Anees <aneesnizam222a@gmail.com>
2025-05-23 14:22:48 +0530

    app.py changed

--- Backend/app.py
+++ Backend/app.py
@@ -127,4 +127,6 @@ def get_profile():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(port=5000, debug=True)
+
+    port = int(os.environ.get('PORT', 5000))  # Use Render's port
+    app.run(host='0.0.0.0', port=port, debug=True)
